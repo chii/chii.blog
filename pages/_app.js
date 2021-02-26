019c19ff5579bb54b5141bb9770734b4d5ded324
Chii <chihironitasaka@gmail.com>
2021-02-26 13:05:25 -0800

    add ga tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Router from 'next/router'
+
+import { GA_TRACKING_ID, pageview } from '../lib/gtag'
+
+if (GA_TRACKING_ID) {
+    Router.events.on('routeChangeComplete', url => pageview(url))
 }
 
-export default MyApp
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+//function MyApp({ Component, pageProps }) {
+//  return <Component {...pageProps} />
+//}
+//
+//export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '../lib/gtag'
+
 class MyDocument extends Document {
     render() {
         return (
@@ -9,6 +11,28 @@ class MyDocument extends Document {
                 * Google Fontsを利用する
               */}
               <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
+              { /* gtag / Google Analyticsを利用する */ }
+              {
+                  GA_TRACKING_ID && <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                  />
+              }
+              {
+                  GA_TRACKING_ID && <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                          window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                        `,
+                    }}
+                  />
+              }
               </Head>
               <body>
                 <Main />
